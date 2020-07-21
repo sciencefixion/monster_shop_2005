cea791b6c0ceffd998a48f0cdc96ecea4e92c497
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-20 19:48:59 -0600

    Add test for email uniqueness and verify its uniqueness.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe User, type: :model do
         it { should validate_presence_of(:zip) }
         it { should validate_presence_of(:email) }
         it { should validate_presence_of(:password) }
+
+        it { should validate_uniqueness_of(:email) }
     end
      
 end
