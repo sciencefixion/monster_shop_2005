cec574c3a2a851b94a1f4858e1a97727c526b636
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-22 16:54:32 -0600

    Add user show page spec for story 19

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "User profile show page" do
+  describe "As a registered user" do
+    before :each do
+      @jim = User.create(name: "Jim", address: "3455 LKV Rd.", city: "Denver", state: "CO", email: "test@test.com", zip: "80124", password: "123456")
+      visit '/login'
+      fill_in :email,	with: "test@test.com"
+      fill_in :password,	with: "123456"
+      click_button "Login"
+    end
+
+    it "text" do
+      visit "/profile"
+
+      expect(page).to have_content("Name: #{@jim.name}")
+      expect(page).to have_content("Address: #{@jim.address}")
+      expect(page).to have_content("City: #{@jim.city}")
+      expect(page).to have_content("State: #{@jim.state}")
+      expect(page).to have_content("zip: #{@jim.zip}")
+      expect(page).to_not have_content(@jim.password)
+      expect(page).to have_link("Edit Profile")
+    end
+  end
+end
