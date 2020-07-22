64919ca7df1c0d03f41e8122d5506059f8cbe114
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-22 14:24:03 -0600

    Add story 16 spec

---
+++ spec/features/sessions/logout_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "User can log out" do
+  before(:each) do
+    @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+    @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+  end
+  it "can log a default user out" do
+    @jim = User.create(name: "Jim", address: "3455 LKV Rd.", city: "Denver", state: "CO", email: "test@test.com", zip: "80124", password: "123456")
+    visit '/login'
+    fill_in :email,	with: "test@test.com"
+    fill_in :password,	with: "123456"
+    click_button "Login"
+    expect(user.role).to eq("default")
+
+    visit "/items/#{@paper.id}"
+    click_on "Add To Cart"
+    visit "/items/#{@pencil.id}"
+    click_on "Add To Cart"
+    expect(page).to have_content("Cart: 2")
+
+    click_on "Logout"
+
+    expect(current_path).to eq("/")
+    expect(page).to have_content("You are logged out")
+    expect(page).to have_content("Cart: 0")
+  end
+end
--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -1,49 +1,51 @@
 require "rails_helper"
 
 RSpec.describe "User session" do
-  it "allows default user to log in" do
-    user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456")
+  describe "Users can login" do
+    it "allows default user to log in" do
+      user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456")
 
-    visit '/login'
+      visit '/login'
 
-    fill_in :email,	with: "test@test.com"
-    fill_in :password,	with: "123456"
-    click_button "Login"
+      fill_in :email,	with: "test@test.com"
+      fill_in :password,	with: "123456"
+      click_button "Login"
 
-    expect(current_path).to eq("/profile")
+      expect(current_path).to eq("/profile")
 
-    expect(page).to have_content("You are now logged in.")
-    expect(user.role).to eq("default")
-  end
+      expect(page).to have_content("You are now logged in.")
+      expect(user.role).to eq("default")
+    end
 
-  it "allows merchant user to log in" do
-    user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 1)
+    it "allows merchant user to log in" do
+      user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 1)
 
-    visit '/login'
+      visit '/login'
 
-    fill_in :email,	with: "test@test.com"
-    fill_in :password,	with: "123456"
-    click_button "Login"
+      fill_in :email,	with: "test@test.com"
+      fill_in :password,	with: "123456"
+      click_button "Login"
 
-    expect(current_path).to eq("/merchant/dashboard")
+      expect(current_path).to eq("/merchant/dashboard")
 
-    expect(page).to have_content("You are now logged in.")
-    expect(user.role).to eq("merchant")
-  end
+      expect(page).to have_content("You are now logged in.")
+      expect(user.role).to eq("merchant")
+    end
 
-  it "allows admin user to log in" do
-    user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 2)
+    it "allows admin user to log in" do
+      user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 2)
 
-    visit '/login'
+      visit '/login'
 
-    fill_in :email,	with: "test@test.com"
-    fill_in :password, with: "123456"
-    click_button "Login"
+      fill_in :email,	with: "test@test.com"
+      fill_in :password, with: "123456"
+      click_button "Login"
 
-    expect(current_path).to eq("/admin/dashboard")
+      expect(current_path).to eq("/admin/dashboard")
 
-    expect(page).to have_content("You are now logged in.")
-    expect(user.role).to eq("admin")
+      expect(page).to have_content("You are now logged in.")
+      expect(user.role).to eq("admin")
+    end
   end
 
   describe "Sad path, bad credentials" do
@@ -60,7 +62,7 @@ RSpec.describe "User session" do
       expect(page).to have_content("Username and/or password are incorrect")
     end
 
-    it "merchant user cannot log in with bad credentials" do
+    it "merchant user cannot log in with bad credentials. Wrong password" do
       user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 1)
 
       visit '/login'
@@ -72,23 +74,5 @@ RSpec.describe "User session" do
       expect(current_path).to eq('/login')
       expect(page).to have_content("Username and/or password are incorrect")
     end
-    
-    it "admin user cannot log in with bad credentials" do
-      user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 2)
-
-      visit '/login'
-
-      fill_in :email,	with: "test@tessfdghsfdgt.com"
-      fill_in :password, with: "1233456"
-      click_button "Login"
-
-      expect(current_path).to eq('/login')
-      expect(page).to have_content("Username and/or password are incorrect")
-    end
   end
 end
-
-
-# If I am a merchant user, I am redirected to my merchant dashboard page
-# If I am an admin user, I am redirected to my admin dashboard page
-# And I see a flash message that I am logged in
