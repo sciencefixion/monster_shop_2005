b94ad94ba6efb5105eb9ec4677cf1f6d8274c007
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-22 14:09:54 -0600

    remove my namespace  admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admin do
-    get "/", to: 'users#index'
-    get "/users", to: 'users#show'
-  end
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
