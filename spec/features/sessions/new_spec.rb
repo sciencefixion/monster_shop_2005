148007d98b46a114019755f3ca5ebe161651db74
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-22 13:50:26 -0600

    Add sad path spec for user story 14

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -45,6 +45,45 @@ RSpec.describe "User session" do
     expect(page).to have_content("You are now logged in.")
     expect(user.role).to eq("admin")
   end
+  
+  it "default user cannot log in with bad credentials" do
+    user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456")
+
+    visit '/login'
+
+    fill_in :email,	with: "test2@test.com"
+    fill_in :password, with: "123456"
+    click_button "Login"
+
+    expect(current_path).to eq('/login')
+    expect(page).to have_content("Username and/or password are incorrect")
+  end
+  #
+  # it "merchant user cannot log in with bad credentials" do
+  #   user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 1)
+  #
+  #   visit '/login'
+  #
+  #   fill_in :email,	with: "test@test.com"
+  #   fill_in :password, with: "1234456"
+  #   click_button "Login"
+  #
+  #   expect(current_path).to eq('/login')
+  #   expect(page).to have_content("Username and/or password are incorrect")
+  # end
+  #
+  # it "admin user cannot log in with bad credentials" do
+  #   user = User.create(name: "Jim", address: "3455 LKV", city: "Hell", state: "MI", email: "test@test.com", zip: "56765", password: "123456", role: 2)
+  #
+  #   visit '/login'
+  #
+  #   fill_in :email,	with: "test@tessfdghsfdgt.com"
+  #   fill_in :password, with: "1233456"
+  #   click_button "Login"
+  #
+  #   expect(current_path).to eq('/login')
+  #   expect(page).to have_content("Username and/or password are incorrect")
+  # end
 end
 
 
