f3f249faeb7850b82815cf33de06cd0f83fb460d
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-27 16:08:49 -0600

    Fixed enum "packaged"

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,12 @@ class Order <ApplicationRecord
   belongs_to :user
   has_many :item_orders
   has_many :items, through: :item_orders
-  
-  enum status: [:pending, :package, :shipped, :cancelled]
+
+  enum status: [:pending, :packaged, :shipped, :cancelled]
 
   def grandtotal
     item_orders.sum('price * quantity')
   end
 end
+
+# Add fullfilled and unfullfilled in the item_orders
