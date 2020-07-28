490e0fa71298939156b71f4546ccd1600bda794f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-28 15:45:47 -0600

    Add logic for enabling merchant.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -5,12 +5,20 @@ class Admin::MerchantsController < Admin::BaseAdminController
 
     def update
         merchant = Merchant.find(params[:merchant_id])
-        merchant.items.each do |item|
-            item.update(enabled?: false)
+        if merchant.enabled
+            merchant.items.each do |item|
+                item.update(enabled?: false)
+            end
+            merchant.update(enabled: false)
+            flash[:success] = "#{merchant.name} has been disabled"
+        else
+            merchant.items.each do |item|
+                item.update({enabled?: true, active?: true})
+            end
+            merchant.update(enabled: true)
+            flash[:success] = "#{merchant.name} has been enabled"
         end
-        merchant.update(enabled: false)
         
-        flash[:success] = "#{merchant.name} has been disabled"
         redirect_to admin_merchants_path
     end
     
