59603451dbd4e2e43517becec416b45ff79edc5d
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-28 14:25:37 -0600

    Add an update action to update it to toggle enaled.

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,5 +2,17 @@ class Admin::MerchantsController < Admin::BaseAdminController
     def index
         @merchants = Merchant.all
     end
+
+    def update
+        merchant = Merchant.find(params[:merchant_id])
+        merchant.items.each do |item|
+            item.update(enabled?: false)
+        end
+        merchant.update(enabled: false)
+        
+        flash[:success] = "#{merchant.name} has been disabled"
+        redirect_to admin_merchants_path
+    end
+    
     
 end
