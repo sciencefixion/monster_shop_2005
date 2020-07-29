b62e84b1e15dc533eea633d477d9fa064b9bd155
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-29 15:49:15 -0600

    Add new and create actions in items.

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -24,6 +24,28 @@ class Merchant::ItemsController < Merchant::BaseMerchantController
         flash[:success] = "Item has been deleted."
         redirect_to merchant_items_path
     end
+
+    def new
+        
+    end
+
+    def create
+        item = current_user.merchant.items.new(item_params)
+        if item.save
+            flash[:success] = 'The Item has been created successfully.'
+            redirect_to merchant_items_path
+        else
+            p "x"
+        end
+        
+    end
+    
+    private
+    def item_params
+        params.permit(:name, :description, :image, :price, :inventory)
+    end
+    
+    
     
     
 end
